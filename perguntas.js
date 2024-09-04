e2d762e8df425d491b2b6a0692ded1d6b8a317e9
pedrodesilva <104081707+pedrodesilva@users.noreply.github.com>
2024-09-04 08:05:15 -0300

    OK

--- perguntas.js
+++ perguntas.js
@@ -1,173 +1,65 @@
 export const perguntas = [
     {
-        enunciado: " ",
+        enunciado: "As produções dos filmes de hoje em dia buscam uma forma mais facil de criar um bom roteiro, buscando inteligencia artificial ",
         alternativas: [
             {
-                texto: "",
+                texto: "isso criaria um vicio por opções mais facil, tirando a graça das produções de roteiro",
                 afirmacao: [
-                    "",
-                    ""
+                    "no inicio achei que iria estragar as obras feitas por humanos",
+                    
                 ],
                 proxima: 1,
             },
             {
-                texto: "",
+                texto: "seria uma forma de criar cenario diferente e fora da caixa",
                 afirmacao: [
-                    "",
-                    ""
+                    "seria legal e algo diferente",
+                    
                 ],
                 proxima: 2,
             },
         ]
     },
     {
-        enunciado: "",
+        enunciado: "como os roteiros foram feitos por uma IA apenas falta os dubladores e os personagens, mas vamos focar nos dubladores que tambem vai ser criada por uma IA",
         alternativas: [
             {
-                texto: "",
+                texto: "vozes artificialmente me da medo",
                 afirmacao: [
-                    "",
-                    "",
-                    ""
+                    "É muito estranho achar normal as falas feitas por uma IA",
+                    
                 ],
                 proxima: 3,
             },
             {
-                texto: "",
+                texto: "imagina criar canarios tecnologicos que precisa usar a IA para criar as falas de robos",
                 afirmacao: [
-                    "",
-                    "",
-                    ""
+                    "seria legal um 'robo' interpretar outro robo",
+                   
                 ],
                 proxima: 4,
             },
         ]
     },
     {
-        enunciado: "",
+        enunciado: "Após o roteiro e dublagem agora imagine a dificuldade de desenhar os personagens e o cenario para criar um bom mangá, então novamente vamos usar IA para ajudar os mangaká  ",
         alternativas: [
             {
-                texto: "",
+                texto: "desenhos feitos por uma IA é muito realista, não tem como achar bonito",
                 afirmacao: [
-                    "",
-                    "",
-                    ""
+                    "os traços são muito detalhados, não tem erros que fazem ser bom para os detalhes",
+                    
                 ],
                 proxima: 3,
             },
             {
-                texto: "",
+                texto: "os traços perfeitos ajudam um mangaká a ter ideias que melhoraria com a IA",
                 afirmacao: [
-                    "",
-                    "",
-                    ""
+                    "seria otimo uma junção dos melhores mangakás com inteligencia artificial",
+                    
                 ],
                 proxima: 4,
             },
         ]
     },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 5,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 6,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 5,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 6,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-                proxima: 7,
-            },
-        ]
-    },
-    {
-        enunciado: "",
-        alternativas: [
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    ""
-                ],
-            },
-            {
-                texto: "",
-                afirmacao: [
-                    "",
-                    "",
-                    ""
-                ],
-            },
-        ]
-    }
-];
+]
